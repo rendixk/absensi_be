f5765bdcab167c83b3390b19fe9893453bc1f7e9
rendixk <ferendfio@gmail.com>
2025-11-24 09:11:32 +0700

    feat: route path

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
 import express from 'express'
+import cors from "cors"
 import { notFoundMiddleware } from './middleware/notFound.middleware'
 import { error_middleware } from './middleware/error.middleware'
+import admin_route from './model/admin/admin_auth/admin.auth.route'
 
 export const app = express()
 
+const cors_option = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
 app.use(express.json())
+app.use(cors(cors_option))
 
 app.get('/', (req, res) => {
   res.send('Hello, World!')
 })
+
+//routing path
+//for admin
+app.use('/admin/auth', admin_route)
+
+
 
 app.use(notFoundMiddleware)
 app.use(error_middleware)
