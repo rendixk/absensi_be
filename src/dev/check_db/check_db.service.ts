07c312c212d85e98b0d3fb3c93e38ce26714656f
rendixk <ferendfio@gmail.com>
2025-12-25 23:36:32 +0700

    delete comment line syntax or unused syntax

--- src/dev/check_db/check_db.service.ts
+++ src/dev/check_db/check_db.service.ts
@@ -1,33 +1,5 @@
 import { prisma } from "../../config/prisma"
 
-// export const check_admin_model_service = () => {
-//     return prisma.admin.findMany()
-// }
-
-// export const check_guru_model_service = () => {
-//     return prisma.guru.findMany()
-// }
-
-// export const check_guru_bk_model_service = () => {
-//     return prisma.guru_bk.findMany()
-// }
-
-// export const check_wali_kelas_model_service = () => {
-//     return prisma.wali_kelas.findMany()
-// }
-
-// export const check_siswa_model_service = () => {
-//     return prisma.siswa.findMany()
-// }
-
-// export const check_kelas_model_service = () => {
-//     return prisma.kelas.findMany({
-//         include: {
-//             wali_kelas: true
-//         }
-//     })
-// }
-
 export class CheckDBService {
     public async Guru() {
         const data_guru = await prisma.guru.findMany({
