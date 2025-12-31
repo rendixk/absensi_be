1c82b82f6efcb572aaa79f6fd3d0ae2b3874309b
rendixk <ferendfio@gmail.com>
2025-12-31 14:04:59 +0700

    fix authorized account

--- src/model/kelas/kelas-detail/kelas-detail.controller.ts
+++ src/model/kelas/kelas-detail/kelas-detail.controller.ts
@@ -10,10 +10,16 @@ export const detail_kelas_controller = async (req: AuthRequest, res: Response, n
             console.log(chalk.redBright("Unauthorized: No user information found in request."))
             throw new ErrorOutput("Unauthorized", 401)
         }
+        if(req.user.role === "siswa") {
+            console.log(chalk.redBright(`[Backend Controller] Forbidden: Role ${req.user.role} is not allowed.`));
+            throw new ErrorOutput("Forbidden: Access denied for your role.", 403);
+        }
 
-        console.log(chalk.cyanBright("Debugging: Request body received for get detail kelas..."))
         const user_id = Number(req.user.id)
+        console.log(chalk.cyanBright(`Fetching detail kelas for User ID: ${user_id}...`))
+
         const detail_kelas = await detail_kelas_service(user_id)
+
         console.log(chalk.greenBright("Detail Kelas fetched successfully."), detail_kelas)
         res.status(200).json({
             success: true,
--- src/model/kelas/kelas-detail/kelas-detail.service.ts
+++ src/model/kelas/kelas-detail/kelas-detail.service.ts
@@ -6,8 +6,16 @@ export const detail_kelas_service = async (user_id: number) => {
     const detail_kelas = await detail_kelas_repo(user_id)
 
     if(!detail_kelas) {
-        console.log(chalk.red("kelas not found"))
+        console.log(chalk.red("[Backend Service] kelas not found"))
         throw new ErrorOutput("Kelas not found", 404)
+    }
+    
+    const active_wali_kelas = detail_kelas.wali_kelas_id === user_id
+    const active_guru = detail_kelas.guru_id === user_id
+
+    if(!active_guru && !active_wali_kelas) {
+        console.log(chalk.redBright("[Backend Service] Access denied: User is not the Wali Kelas or active Guru"))
+        throw new ErrorOutput("You don't have active access to this class. Please login first.", 401)
     }
 
     return detail_kelas
