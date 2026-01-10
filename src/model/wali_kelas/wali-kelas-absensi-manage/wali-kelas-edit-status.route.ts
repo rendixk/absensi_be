a848f409856fc260fcf130997fbc84472e16031a
rendixk <ferendfio@gmail.com>
2026-01-10 09:57:44 +0700

    change dir path

--- src/model/wali_kelas/wali-kelas-absensi-manage/wali-kelas-edit-status.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { wali_kelas_edit_status_controller } from "./wali-kelas-edit-status.controller"
-import { auth_token_middleware } from "../../../middleware/auth.middleware"
-
-const router = Router()
-
-router.put('/edit', auth_token_middleware, wali_kelas_edit_status_controller)
-
-export default router
